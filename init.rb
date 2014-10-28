445fd7064e6ff2bcf3a54db149a56a653d00bbda
Zinway Liu <zinway.liu@gmail.com>
2014-10-28 17:24:08 +0800

    Version 1.1.0

--- init.rb
+++ init.rb
@@ -9,9 +9,9 @@ Redmine::Plugin.register :redmine_ckeditor do
   name 'Redmine CKEditor plugin'
   author 'Akihiro Ono'
   description 'This is a CKEditor plugin for Redmine'
-  version '1.0.16'
+  version '1.1.0'
   requires_redmine :version_or_higher => '2.3.0'
-  url 'http://github.com/a-ono/redmine_ckeditor'
+  url 'https://github.com/zinw/redmine_ckeditor'
 
   settings(:partial => 'settings/ckeditor')
 
